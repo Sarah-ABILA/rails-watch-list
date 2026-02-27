65985bc3277ab5283635186ab4fe61b4b356baef
Sarah Abila <abila.sarah83@gmail.com>
2026-02-27 09:02:38 +0100

    watch list bookmarks ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
   end
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
@@ -12,5 +14,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   resources :bookmarks, only: [:destroy]
   # Defines the root path route ("/")
-  # root "posts#index"
+   root to: "posts#index"
 end
